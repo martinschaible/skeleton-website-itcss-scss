851d279f4256b13f03fdda0e0778cb2bc836d38b
Martin Schaible <martin@martinschaible.ch>
2024-05-01 22:29:25 +0200

    Working copies of the JS configuration files

---
+++ src/js/index.umd.js
@@ -0,0 +1,34 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap index.umd.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from '../vendor/bootstrap/js/src/alert.js'
+import Button from '../vendor/bootstrap/js/src/button.js'
+import Carousel from '../vendor/bootstrap/js/src/carousel.js'
+import Collapse from '../vendor/bootstrap/js/src/collapse.js'
+import Dropdown from '../vendor/bootstrap/js/src/dropdown.js'
+import Modal from '../vendor/bootstrap/js/src/modal.js'
+import Offcanvas from '../vendor/bootstrap/js/src/offcanvas.js'
+import Popover from '../vendor/bootstrap/js/src/popover.js'
+import ScrollSpy from '../vendor/bootstrap/js/src/scrollspy.js'
+import Tab from '../vendor/bootstrap/js/src/tab.js'
+import Toast from '../vendor/bootstrap/js/src/toast.js'
+import Tooltip from '../vendor/bootstrap/js/src/tooltip.js'
+
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+}
